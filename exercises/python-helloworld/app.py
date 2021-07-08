3e7a193191ac97841967b1f09f0a76d3a1a09146
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-07-08 03:51:10 -0300

    feat: adding application logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, json
 
 app = Flask(__name__)
@@ -6,6 +7,10 @@ app = Flask(__name__)
 @app.route('/status')
 def healthcheck():
     data = {'result': 'OK - healthy'}
+
+    # log line
+    app.logger.info('Status request successfull')
+
     return app.response_class(
         response=json.dumps(data),
         status=200,
@@ -23,6 +28,10 @@ def metrics():
             'UserCountActive': 23
         }
     }
+
+    # log line
+    app.logger.info('Metrics request successfull')
+
     return app.response_class(
         response=json.dumps(data),
         status=200,
@@ -32,8 +41,14 @@ def metrics():
 
 @app.route('/')
 def hello():
+    # log line
+    app.logger.info('Main request successfull')
+
     return 'Hello World!'
 
 
 if __name__ == '__main__':
+    # stream logs to a file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
     app.run(host='0.0.0.0')
