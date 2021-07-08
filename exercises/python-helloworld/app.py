517a2ad188132c6b3a7c90ae4cd967307de976f7
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-07-08 03:35:36 -0300

    feat: adding endpoint for application status

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, json
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
+@app.route('/status')
+def healthcheck():
+    data = {'result': 'OK - healthy'}
+    return app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/metrics')
+def metrics():
+    data = {
+        'status': 'success',
+        'code': 0,
+        'data': {
+            'UserCount': 140,
+            'UserCountActive': 23
+        }
+    }
+    return app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
